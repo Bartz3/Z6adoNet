cbd17fa5d8a30b9ea10cbfb9ac039735f9c87eda
Bartek Późniewski <bartek.pozniewski@wp.pl>
2022-04-23 13:24:20 +0200

    Add SP added

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿DROP PROCEDURE sp_productAdd
+GO
+CREATE PROCEDURE [dbo].[sp_productAdd]
+@name VARCHAR (50),
+@price MONEY,
+@productID int OUTPUT
+AS
+INSERT INTO Product (name, price) VALUES (@name, @price)
+SET @productID = @@IDENTITY
--- Z6adoNet/Pages/Create.cshtml.cs
+++ Z6adoNet/Pages/Create.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
 using System.Data.SqlClient;
 using Zadanie6.Models;
 namespace Zadanie6.Pages
@@ -24,37 +25,57 @@ namespace Zadanie6.Pages
         {
             p.name = newProduct.name;
             p.price = newProduct.price;
-            string myCompanyDBcs = _configuration.GetConnectionString("MyCompanyDB");
-            SqlConnection con = new SqlConnection(myCompanyDBcs);
-            //string sql = "INSERT INTO Product VALUES(5,'"+p.name+"','"+ p.price.ToString()+"')";
-            // string sql = "INSERT INTO Product (name,price) VALUES('"+p.name+"','"+ p.price.ToString()+"')";
-            string sql = "INSERT INTO Product (name,price) VALUES(@name,@price)";
 
-            SqlCommand cmd = new SqlCommand(sql, con);
-            cmd.Parameters.AddWithValue("@name", p.name);
-            cmd.Parameters.AddWithValue("@price", p.price);
-            try
-            {
-                con.Open();
-                int numAff = cmd.ExecuteNonQuery();
-                //lblInfoText += string.Format("<br />Deleted <b>{0}</b> record(s)<br />", numAff);
-            }
-            catch (SqlException exc)
-            {
-                //lblInfoText += string.Format("<b>Error:</b> {0}<br /><br />", exc.Message);
-            }
-            finally { con.Close(); }
-            //using (con)
-            //{
-            //    con.Open();
-            //    SqlCommand cmd = new SqlCommand(sql, con);
-            //    SqlDataReader reader = cmd.ExecuteReader();
-            //    reader.Close();
-            //    con.Close();
-            //}
-            
+            string myCompanyDBcs =
+           _configuration.GetConnectionString("myCompanyDB");
+
+            SqlConnection con = new SqlConnection(myCompanyDBcs);
+            SqlCommand cmd = new SqlCommand("sp_productAdd", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlParameter name_SqlParam = new SqlParameter("@name", SqlDbType.VarChar,
+           50);
+            name_SqlParam.Value = p.name;
+            cmd.Parameters.Add(name_SqlParam);
+            SqlParameter price_SqlParam = new SqlParameter("@price", SqlDbType.Money);
+            price_SqlParam.Value = p.price;
+            cmd.Parameters.Add(price_SqlParam);
+
+            SqlParameter productID_SqlParam = new SqlParameter("@productID",
+           SqlDbType.Int);
+            productID_SqlParam.Direction = ParameterDirection.Output;
+            cmd.Parameters.Add(productID_SqlParam);
+            con.Open();
+            int numAff = cmd.ExecuteNonQuery();
+            con.Close();
 
             return RedirectToPage("List");
         }
     }
 }
+//public IActionResult OnPost(Product p)
+//{
+//    p.name = newProduct.name;
+//    p.price = newProduct.price;
+//    string myCompanyDBcs = _configuration.GetConnectionString("MyCompanyDB");
+//    SqlConnection con = new SqlConnection(myCompanyDBcs);
+//    //string sql = "INSERT INTO Product VALUES(5,'"+p.name+"','"+ p.price.ToString()+"')";
+//    // string sql = "INSERT INTO Product (name,price) VALUES('"+p.name+"','"+ p.price.ToString()+"')";
+//    string sql = "INSERT INTO Product (name,price) VALUES(@name,@price)";
+
+//    SqlCommand cmd = new SqlCommand(sql, con);
+//    cmd.Parameters.AddWithValue("@name", p.name);
+//    cmd.Parameters.AddWithValue("@price", p.price);
+//    try
+//    {
+//        con.Open();
+//        int numAff = cmd.ExecuteNonQuery();
+//        //lblInfoText += string.Format("<br />Deleted <b>{0}</b> record(s)<br />", numAff);
+//    }
+//    catch (SqlException exc)
+//    {
+//        //lblInfoText += string.Format("<b>Error:</b> {0}<br /><br />", exc.Message);
+//    }
+//    finally { con.Close(); }
+
+//    return RedirectToPage("List");
+//}
