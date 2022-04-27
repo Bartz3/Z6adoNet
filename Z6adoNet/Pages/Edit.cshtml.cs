28e9ce9412f33bd4eaf049ea81ebfd2863ed819a
Bartek Późniewski <bartek.pozniewski@wp.pl>
2022-04-27 20:13:27 +0200

    CategoryList done, List display with Id of category(not longName)

--- Z6adoNet/Pages/Edit.cshtml.cs
+++ Z6adoNet/Pages/Edit.cshtml.cs
@@ -9,10 +9,35 @@ namespace Zadanie6.Pages
     {
         [BindProperty(SupportsGet = true)]
         public Product editProduct { get; set; }
+
+        public List<Category> categoryList = new List<Category>();
+
+        [BindProperty]
+        public Category category { get; set; }
         public void OnGet(int id)
         {
             editProduct.id  = id;
-            //editProduct.name = _name;
+
+            string myCompanyDBcs = _configuration.GetConnectionString("MyCompanyDB");
+
+            SqlConnection con = new SqlConnection(myCompanyDBcs);
+            SqlCommand cmd = new SqlCommand("sp_categoryDisplay", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            Category _category;
+            while (reader.Read())
+            {
+                _category = new Category();
+                _category.id = int.Parse(reader["Id"].ToString());
+                _category.shortName = reader["shortName"].ToString();
+                _category.longName = reader["longName"].ToString();
+
+                categoryList.Add(_category);
+            }
+            reader.Close(); con.Close();
         }
         public IConfiguration _configuration { get; }
         private readonly ILogger<EditModel> _logger;
@@ -26,6 +51,7 @@ namespace Zadanie6.Pages
             p.id = editProduct.id;
             p.name = editProduct.name;
             p.price = editProduct.price;
+            p.CategoryID = editProduct.CategoryID;
 
 
             string myCompanyDBcs = _configuration.GetConnectionString("MyCompanyDB");
@@ -48,6 +74,12 @@ namespace Zadanie6.Pages
             //productID_SqlParam.Direction = ParameterDirection.Input;
             productID_SqlParam.Value = p.id;
             cmd.Parameters.Add(productID_SqlParam);
+
+            SqlParameter categoryID_SqlParam = new SqlParameter("@categoryId",
+            SqlDbType.Int);
+            //productID_SqlParam.Direction = ParameterDirection.Input;
+            categoryID_SqlParam.Value = p.CategoryID;
+            cmd.Parameters.Add(categoryID_SqlParam);
 
             con.Open();
             int numAff = cmd.ExecuteNonQuery();
