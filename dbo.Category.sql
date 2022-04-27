4e935d6bc0506731775b54bf000aa8528c689c26
Bartek Późniewski <bartek.pozniewski@wp.pl>
2022-04-27 20:56:12 +0200

    Tables added

---
+++ dbo.Category.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Category] (
+    [Id]        INT        IDENTITY (1, 1) NOT NULL,
+    [shortName] NCHAR (30) NOT NULL,
+    [longName]  NCHAR (80) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ dbo.Product.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Product] (
+    [Id]         INT        IDENTITY (1, 1) NOT NULL,
+    [name]       NCHAR (50) NOT NULL,
+    [price]      MONEY      NOT NULL,
+    [categoryId] INT        NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
