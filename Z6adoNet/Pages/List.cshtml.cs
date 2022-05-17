1abb66b4a1479f98d92cc19f3f151c8c6c809f1e
Bartek Późniewski <bartek.pozniewski@wp.pl>
2022-05-17 12:18:53 +0200

    e

--- Z6adoNet/Pages/List.cshtml.cs
+++ Z6adoNet/Pages/List.cshtml.cs
@@ -43,7 +43,8 @@ namespace Zadanie6.Pages
                       _product.description = reader["Description"].ToString();
                 }
                 _product.price = Decimal.Parse(reader["Price"].ToString());
-                
+                _product.longName = reader["longName"].ToString();
+
                 productList.Add(_product);
             }
             reader.Close(); con.Close();      
---
+++ dbo.sp_productDisplay.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [dbo].[sp_productDisplay]
+AS
+SELECT p.id,p.name,p.price,p.categoryId,c.longName,c.Id 
+FROM Product p,Category c
+WHERE p.categoryId=c.Id
